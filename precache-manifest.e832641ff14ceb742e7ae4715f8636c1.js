41e5c723f2ce19d53bed112aa290e8be634d5325
khalilalhraky <khalilalhraky@gmail.com>
2020-08-09 20:35:47 +0300

    Updates

---
+++ precache-manifest.e832641ff14ceb742e7ae4715f8636c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909f82ff2d80444158827d388961cb9d",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "e3cfa3a3a4c94db34ec4",
+    "url": "/website/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fc0ee17dc7e50e7c23e9",
+    "url": "/website/static/js/2.6847c54c.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/website/static/js/2.6847c54c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3cfa3a3a4c94db34ec4",
+    "url": "/website/static/js/main.c28cb2d4.chunk.js"
+  },
+  {
+    "revision": "c0e7a96e1e1fafc71966",
+    "url": "/website/static/js/runtime-main.c0bafae9.js"
+  }
+]);
